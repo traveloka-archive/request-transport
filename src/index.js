fe654d2c8f6907a2b591d397a855efb5d6a73dea
Asendia Mayco <asendia@traveloka.com>
2015-08-19 23:23:22 +0700

    Fix package.json and export

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export { default as DomainRouter } from './router/DomainRouter';
 export { default as PathPrefixRouter } from './router/PathPrefixRouter';
+export { default as Page } from './core/Page';
+export { default as Filter } from './core/Filter';
