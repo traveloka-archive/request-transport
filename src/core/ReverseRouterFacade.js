79eee9650d46562263f33c00631601288efec7ba
Asendia Mayco <asendia@traveloka.com>
2016-02-24 00:41:03 +0700

    Fix wrong param sent to reverseRouter

--- src/core/ReverseRouterFacade.js
+++ src/core/ReverseRouterFacade.js
@@ -5,12 +5,12 @@ class ReverseRouterFacade {
     this._locale = locale;
   }
 
-  url(pageId, params, locale = this._locale, protocol = this._protocol) {
-    return this._reverseRouter.url(pageId, locale, params, protocol);
+  url(pageId, params, locale = this._locale, queryString, protocol = this._protocol) {
+    return this._reverseRouter.url(pageId, locale, params, queryString, protocol);
   }
 
   urlWithLocale(pageId, locale, protocol = this._protocol) {
-    return this._reverseRouter.url(pageId, locale, undefined, protocol);
+    return this._reverseRouter.url(pageId, locale, undefined, undefined, protocol);
   }
 }
 
