2298e21d09e97c9d1834ce2ee65090ce1fb0985b
Asendia Mayco <asendia@traveloka.com>
2019-09-19 15:34:29 +0700

    Fix multiple res.write calls

--- src/core/asInterceptor.js
+++ src/core/asInterceptor.js
@@ -5,13 +5,31 @@
 export default function asInterceptor(fn) {
   const interceptor = function (req, res, next) {
     const originalEnd = res.end;
+    const originalWrite = res.write;
 
-    res.end = function end (chunk, encoding) {
-      const args = Array.prototype.slice.call(arguments);
+    res.end = function end (...args) {
+      // This flag ensure original res.end to be called after
+      // all fiters have been executed.
+      if (res._filterHasBeenExecutedBefore) {
+        return originalEnd.apply(res, args);
+      }
       fn(req, res, () => {
         originalEnd.apply(res, args);
       });
+      res._filterHasBeenExecutedBefore = true;
     };
+
+    res.write = function write (...args) {
+      // This flag ensure all onResponseFilters to be executed
+      // before calling res.write.
+      if (res._filterHasBeenExecutedBefore) {
+        return originalEnd.apply(res, args);
+      }
+      fn(req, res, () => {
+        originalWrite.apply(res, args);
+      });
+      res._filterHasBeenExecutedBefore = true;
+    }
 
     next();
   };
